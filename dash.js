e3158981eaf78c64074fb0ed16421616af1b30c9
Moshe Ezderman <moshe.ezderman@whiteops.com>
2018-11-03 08:03:01 -0400

    add dash

--- dash.js
+++ dash.js
@@ -1,6 +1,7 @@
 var dash_button = require('node-dash-button'),
-    dash = dash_button('ac:63:be:2b:7f:c9'), 
+  dash = dash_button('ac:63:be:2b:7f:c9'), // replace xx:xx:xx:xx:xx:xx with your Dash Button's hardware address
+  exec = require('child_process').exec
 
-dash.on('detected', function() {
-   console.log('I got dash message')
-});
+dash.on('detected', function () {
+  console.log('I got dash message')
+})
